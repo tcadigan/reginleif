ab6225cde47ef7ecd55eea8629d6444f8fe7b8bd
Tyler Cadigan <tylercadigan@gmail.com>
2015-02-19 23:14:59 -0700

    remove instadeath

--- main.c
+++ main.c
@@ -283,10 +283,6 @@ int main(int argc, char **argv, char **envp)
     obj->o_which = 0;
     add_pack(item, TRUE);
 
-    /* TC_DEBUG: Start */
-    death('W');
-    /* TC_DEBUG: Finish */
-
     playit();
 
     return 0;
