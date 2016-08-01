efcac19019f296bf1a5a4d013de775278b755d70
Tyler Cadigan <tylercadigan@gmail.com>
2016-07-31 22:58:12 -0700

    a couple files done

---
+++ client/source/csp.h
@@ -0,0 +1,196 @@
+/*
+ * csp.h: Contains the defines for all the Client-Server Protocol (CSP) numbers.
+ *
+ * CSP, copyright (c) 1993 by John P. Deregon, Evan Koffler
+ *
+ * #ident  "@(#)cps.h   1.1 11/20/93 "
+ *
+ * Please send any modifications of this file to:
+ *    evank@netcom.com
+ *    deragon@jethro.nyu.edu
+ */
+
+/*---- Server responses ----*/
+
+/* VERSION */
+#define CSP_VERSION_INFO    10 /* Version XX */
+#define CSP_VERSION_OPTIONS 11 /* Options set YY */
+
+/* LOGIN */
+#define CSP_CLIENT_ON  30 /* Client mode on */
+#define CSP_CLIENT_OFF 31 /* Client mode off */
+#define CSP_KNOWLEDGE  32
+
+/* INFO */
+#define CSP_SCOPE_PROMPT 40
+
+/* UPDATE/SEGMENT/RESET */
+#define CSP_UPDATE_START      50 /* Update started */
+#define CSP_UPDATE_END        51 /* Update finished */
+#define CSP_SEGMENT_START     52 /* Segment started */
+#define CSP_SEGMENT_END       53 /* Segment finished */
+#define CSP_RESET_START       54 /* Reset started */
+#define CSP_RESET_END         55 /* Reset finished */
+#define CSP_BACKUP_START      56 /* Backup started */
+#define CSP_BACKUP_END        57 /* Backup finished */
+#define CSP_UPDATES_SUSPENDED 58 /* Updates suspended */
+#define CSP_UPDATES_RESUMED   59 /* Updates resumed */
+#define CSP_PING              60 /* Ping pong */
+#define CSP_PAUSE             61 /* Pause display */
+
+/* SURVEY */
+#define CSP_SURVEY_INTRO  101 /* Planet info */
+#define CSP_SURVEY_SECTOR 102 /* Sector info */
+#define CSP_SURVEY_END    103 /* End of command (EOC) */
+
+/* RELATION */
+#define CSP_RELATION_INTRO 201 /* Race # and name */
+#define CSP_RELATION_DATA  202 /* The data */
+#define CSP_RELATION_END   203 /* End of command (EOC) */
+
+/* 
+ * PROFILE
+ * DYNAMIC = Active Knowledge Capital Morale Cun GTele STele 
+ * DYNAMIC_OTHER = %Know Morale Gun GTele OTele SecPref
+ */
+#define CSP_PROFILE_INTRO         301 /* Header */
+#define CSP_PROFILE_PERSONAL      302
+#define CSP_PROFILE_DYNAMIC       303
+#define CSP_PROFILE_DYNAMIC_OTHER 304
+#define CSP_PROFILE_RACE_STATS    305
+#define CSP_PROFILE_PLANET        306
+#define CSP_PROFILE_SECTOR        307
+#define CSP_PROFILE_DISCOVERY     308 /* Discoveries */
+#define CSP_PROFILE_END           309
+
+/* WHO */
+#define CSP_WHO_INTRO   401 /* Header */
+#define CSP_WHO_DATA    402 /* Actual data */
+#define CSP_WHO_COWARDS 403 /* Send either # of cowards or WHO_END as terminator */
+#define CSP_WHO_END     403 /* Send either # of cowards or WHO_END as terminator */
+
+/*
+ * EXPLORE
+ * STAR_DATA = # Name Ex Inhab Auto Slaved Toxic Compat Type
+ */
+#define CSP_EXPLORE_INTRO       501
+#define CSP_EXPLORE_STAR        502
+#define CSP_EXPLORE_STAR_ALIENS 503
+#define CSP_EXPLORE_STAR_DATA   504
+#define CSP_EXPLORE_STAR_END    505
+#define CSP_EXPLORE_END         506
+
+/*
+ * MAP
+ * DYNAMIC_1 = Type Sects Guns MobPoints Res Des Fuel Xtals
+ * DYNAMIC_2 = Mob AMob Compat Pop ^Pop ^TPop Mil Tax ATax Deposits Est Prod
+ */
+#define CSP_MAP_INTRO     601
+#define CSP_MAP_DYNAMIC_1 602
+#define CSP_MAP_DYNAMIC_2 603
+#define CSP_MAP_ALIENS    604
+#define CSP_MAP_DATA      605
+#define CSP_MAP_END       606
+
+/* CLIENT GENERATED COMMANDS */
+#define CSP_LOGIN_COMMAND    1101 /* Login command */
+#define CSP_VERSION_COMMAND  1102 /* Version command */
+#define CSP_SURVEY_COMMAND   1103 /* Imap command */
+#define CSP_RELATION_COMMAND 1104 /* Relation command */
+#define CSP_PROFILE_COMMAND  1105 /* Profile command */
+#define CSP_WHO_COMMAND      1106 /* Who command */
+#define CSP_EXPLORE_COMMAND  1107 /* Exploration command */
+#define CSP_MAP_COMMAND      1108 /* Map command */
+#define CSP_SCOPE_COMMAND    1110 /* Request a prompt */
+#define CSP_SHIPLIST_COMMAND 1111 /* Request ship list */
+#define CSP_STARDUMP_COMMAND 1112 /* Request star dump */
+#define CSP_SECTORS_COMMAND  1113 /* Request sector count */
+#define CSP_UNIVDUMP_COMMAND 1114 /* Request a universe dump */
+
+/*---- Error Responses ----*/
+#define CSP_ERR                 9900 /* Error */
+#define CSP_ERR_TOO_MANY_ARGS   9901 /* Too many args */
+#define CSP_ERR_TOO_FEW_ARGS    9902 /* Too few args */
+#define CSP_ERR_UNKNOWN_COMMAND 9903 /* Unknown command */
+#define CSP_ERR_NOSUCH_PLAYER   9904 /* No such player */
+#define CSP_ERR_NOSUCH_PLACE    9905 /* No such place - scope error */
+
+#define CSP_MAX_SERVER_COMMAND 1110
+
+/* Do we need these at all? I do not! */
+#define RACE_TYPE_UNKNOWN  0
+#define RACE_TYPE_MORPH    1
+#define RACE_TYPE_NORMAL   2
+#define RELAT_UNKNOWN      0
+#define RELAT_ALLIED       1
+#define RELAT_NEUTRAL      2
+#define RELAT_WAR          3
+#define CSP_ZOOM          35
+
+/* Broadcast/Think/Announce/Shout */
+#define CSP_BROADCAST 802 /* Broadcast */
+#define CSP_ANNOUNCE  803 /* Announce */
+#define CSP_THINK     804 /* Think */
+#define CSP_SHOUT     805 /* Shout */
+#define CSP_EMOTE     805 /* Emote */
+
+#define CSP_ORBIT_COMMAND    1501 /* Orbit command */
+#define CSP_ZOOM_COMMAND     1503 /* Zoom command */
+#define CSP_PLANDUMP_COMMAND 1503 /* Planet dump command */
+#define CSP_SHIPDUMP_COMMAND 1504 /* Ship dump command */
+
+/* Planet Dumps */
+#define CSP_PLANDUMP_INTRO      2000 /* Planet name */
+#define CSP_PLANDUMP_CONDITIONS 2001 /* Conditions */
+#define CSP_PLANDUMP_STOCK      2002 /* Stockpiles */
+#define CSP_PLANDUMP_PROD       2003 /* Production last update */
+#define CSP_PLANDUMP_MISC       2004 /* Rest of stuff */
+#define CSP_PLANDUMP_NOEXPL     2005 /* Planet not explored */
+#define CSP_PLANDUMP_END        2009 /* Sent when done */
+
+/* General usage */
+#define CSP_START_ENEXPL 2010 /* Star is not explored */
+
+/* ORBIT */
+#define CSP_ORBIT_OUTPUT_INTRO  2020 /* Orbit parameters */
+#define CSP_ORBIT_STAR_DATA     2021 /* Star info */
+#define CSP_ORBIT_UNEXP_PL_DATA 2022 /* Unexplored planet info */
+#define CSP_ORBIT_EXP_PL_DATA   2023 /* Explored planet info */
+#define CSP_ORBIT_SHIP_DATA     2024 /* Ship info */
+#define CSP_ORBIT_OUTPUT_END    2025 /* End of command (EOC) */
+
+/* Ship dumps */
+#define CSP_SHIPDUMP_GEN         2030 /* General information */
+#define CSP_SHIPDUMP_STOCK       2031 /* Stock information */
+#define CSP_SHIPDUMP_STATUS      2032 /* Status information */
+#define CSP_SHIPDUMP_WEAPONS     2033 /* Weapons information */
+#define CSP_SHIPDUMP_FACTORY     2034 /* Factory information */
+#define CSP_SHIPDUMP_DEST        2035 /* Destination information */
+#define CSP_SHIPDUMP_PTACT_GEN   2036 /* General planet tactical */
+#define CSP_SHIPDUMP_PTACT_PDIST 2037 /* Distance between planets */
+#define CSP_SHIPDUMP_STACT_PDITS 2038 /* Distance between a ship */
+#define CSP_SHIPDUMP_PTACT_INFO  2039 /* For a ship from a planet */
+#define CSP_SHIPDUMP_STACT_INFO  2040 /* For a ship from a ship */
+#define CSP_SHIPDUMP_ORDERS      2041 /* Ship orders */
+#define CSP_SHIPDUMP_THRESH      2042 /* Ship threshloading */
+#define CSP_SHIPDUMP_SPECIAL     2043 /* Ship specials */
+#define CSP_SHIPDUMP_HYPER       2044 /* Hyper drive usage */
+#define CSP_SHIPDUMP_END         2055 /* End of command (EOC) */
+
+/* Ship list */
+#define CSP_SHIPLIST_INTRO 3000 /* General info */
+#define CSP_SHIPLIST_DATA  3001 /* A shipno - this will repeat for each ship in star */
+#define CSP_SHIPLIST_END   3002 /* Don. In case client is waiting for a signal */
+
+#define CSP_STARDUMP_INTRO     4000
+#define CSP_STARDUMP_CONDITION 4001
+#define CSP_STARDUMP_PLANET    4002
+#define CSP_STARDUMP_END       4003
+#define CSP_STARDUMP_WORMHOLE  4004
+
+#define CSP_UNIVDUMP_INTRO 4010
+#define CSP_UNIVDUMP_STAR  4011
+#define CSP_UNIVDUMP_END   4012
+
+#define CSP_SECTORS_INTRO 4100
+#define CSP_SECTORS_END   4101
---
+++ client/source/csp_types.h
@@ -0,0 +1,104 @@
+/*
+ * csp_types.h: Variables and defines used internally in CSP related functions
+ *
+ * CSP, copyright (c) 1993 by John P. Deragon, Evan Koffler
+ *
+ * Please send any modifications of this file to:
+ *    evank@netcom.com
+ *    deragon@jethro.nyu.edu
+ *
+ * #ident  "@(#)csp_types.h     i.1 11/20/93 "
+ */
+
+#ifndef _CSP_TYPES_H_
+#define _CSP_TYPES_H_
+
+/*
+ * Character to be found in column 0 of a CSP output line send by server to
+ * client
+ */
+#define CSP_CLIENT '|'
+
+/* Identifier sent by client to server when sending a command/request */
+#define CSP_SERVER "CSP"
+
+/* Maximum depth of scope, i.e. nested ships */
+#define CPSD_MAXSHIP_SCOPE 10
+#define CPSD_NOSHIP         0
+
+/* Maximum number of unique letters in star and planet names */
+#define MAX_SCOPE_LTRS 4
+
+/* Levels for sending the prompt in CSP_PROMPT */
+enum LOCATION {
+    CSPD_UNIV, /* 0 */
+    CSPD_STAR, /* 1 */
+    CSPD_PLAN, /* 2 */
+    CSPD_LOCATION_UNKNOWN = 99
+};
+
+/* Used in CSP_PROFILE among other places */
+enum RACE_TYPE {
+    CSPD_RACE_UNKNOWN, /* 0 */
+    CSPD_RACE_MORPH,   /* 1 */
+    CSPD_RACE_NORMAL   /* 2 */
+};
+
+/* Used in CSP_PROFILE and CSP_RELATION */
+enum RELATION {
+    CSPD_RELAT_UNKNOWN, /* 0 */
+    CSPD_RELAT_ALLIED,  /* 1 */
+    CSPD_RELAT_NEUTRAL, /* 2 */
+    CSPD_RELAT_WAR      /* 3 */
+};
+
+/* Used in CSP_PROFILE for discoveries */
+enum DISCOVERIES {
+    CSPD_HYPERDRIVE, /* 0 */
+    CSPD_CRYSTAL,    /* 1 */
+    CSPD_LASER,      /* 2 */
+    CSPD_CEW,        /* 3 */
+    CSPD_AVPM,       /* 4 */
+    CSPD_MAX_NUM_DISCOVERY,
+    CSPD_DISCOVERY_UNKNOWN = 99
+};
+
+enum PLAYER_TYPE {
+    CSPD_NORMAL, /* 0 */
+    CSPD_DIETY,  /* 1 */
+    CSPD_GUEST   /* 2 */
+};
+
+enum SECTOR_TYPES {
+    CSPD_SECTOR_SEA,
+    CSPD_SECTOR_LAND,
+    CSPD_SECTOR_MOUNT,
+    CSPD_SECTOR_GAS,
+    CSPD_SECTOR_ICE,
+    CSPD_SECTOR_FOREST,
+    CSPD_SECTOR_DESERT,
+    CSPD_SECTOR_PLATED,
+    CSPD_SECTOR_WASTED,
+    CSPD_SECTOR_UNKNOWN
+};
+
+enum PLANET_TYPES {
+    CSPD_PLANET_CLASS_M,
+    CSPD_PLANET_ASTEROID,
+    CSPD_PLANET_AIRLESS,
+    CSPD_PLANET_ICEBALL,
+    CPSD_PLANET_JOVIAN,
+    CPSD_PLANET_WATERBALL,
+    CSPD_PLANET_FOREST,
+    CSPD_PLANET_DESERT
+};
+
+enum COMMUNICATION {
+    CSPD_BROADCAST,
+    CSPD_ANNOUNCE,
+    CSPD_THINK,
+    CSPD_SHOUT,
+    CSPD_EMOTE
+};
+
+#endif /* _CSP_TYPES_H_ */
